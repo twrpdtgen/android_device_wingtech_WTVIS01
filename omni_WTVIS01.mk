27dba1430d435ba89c9effd1d79ab1cde725d10a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 06:56:12 +0100

    WTVIS01: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: wingtech Model: WTVIS01 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_WTVIS01.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from WTVIS01 device
+$(call inherit-product, device/wingtech/WTVIS01/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := WTVIS01
+PRODUCT_NAME := omni_WTVIS01
+PRODUCT_BRAND := Visible
+PRODUCT_MODEL := WTVIS01
+PRODUCT_MANUFACTURER := wingtech
